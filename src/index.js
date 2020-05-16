4c83a3aab243fe2d1683351c053fc28e02df60a5
dkwbt <4709696+dkwbt@users.noreply.github.com>
2020-05-16 23:43:51 +0900

    implement event new component

--- src/index.js
+++ src/index.js
@@ -6,14 +6,21 @@ import thunk from 'redux-thunk';
 import './index.css';
 import reducer from './reducers';
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new'
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <EventsIndex />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/events/new" component={EventsNew} />
+          <Route exact path="/" component={EventsIndex} />
+        </Switch>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
